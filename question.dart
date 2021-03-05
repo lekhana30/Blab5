beefa1922287a528d8cd1f1d1d9593ca478932dc
Lekhana S <55750780+lekhana30@users.noreply.github.com>
2021-03-05 23:48:19 +0530

    Create question.dart

---
+++ question.dart
@@ -0,0 +1,10 @@
+---comment Left pane > lib > new > dartfile > question.dart----
+  
+question.dart: 
+
+
+class Question {
+ final String questionText;
+ final bool answer;
+ Question({this.questionText, this.answer});
+}
